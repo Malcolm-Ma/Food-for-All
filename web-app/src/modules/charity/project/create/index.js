fa32a6506992bc6f0efbf00828d41ca5163e6b57
Shi Tianhao <tshi14@sheffield.ac.uk>
2022-03-17 10:23:26 +0000

    Update Create Project

--- web-app/src/modules/charity/project/create/index.js
+++ web-app/src/modules/charity/project/create/index.js
@@ -5,16 +5,88 @@
 
 import { useSelector } from 'react-redux';
 import { useEffect } from "react";
+import React from 'react';
+import {
+  Form,
+  Input,
+  InputNumber,
+  Select,
+  Button,
+  DatePicker,
+} from 'antd';
+
+const { RangePicker } = DatePicker;
+const { Option } = Select;
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+
+const validateMessages = {
+  required: '${label} is required!',
+  types: {
+    email: '${label} is not a valid email!',
+    number: '${label} is not a valid number!',
+  },
+  number: {
+    range: '${label} must be between ${min} and ${max}',
+  },
+};
+
+const suffixSelector = (
+  <Form.Item name="suffix" noStyle>
+    <Select style={{ width: 100 }}>
+      <Option value="USD">$ USD</Option>
+      <Option value="CNY">¥ CNY</Option>
+    </Select>
+  </Form.Item>
+);
+
+const rangeConfig = {
+  rules: [{ type: 'array', required: true, message: 'Please select time!' }],
+};
+
 
 export default () => {
-
   const { userInfo } = useSelector(state => state.user);
 
   useEffect(() => {
     console.log('--userInfo--\n', userInfo);
   }, [userInfo]);
 
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
   return (
-    <div></div>
+    <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
+
+      <Form.Item
+        name="donation"
+        label="Donation"
+        rules={[{ required: true, message: 'Please input donation amount!' }]}
+      >
+        <InputNumber addonAfter={suffixSelector} style={{ width: '100%' }} />
+      </Form.Item>
+
+      <Form.Item name="range-picker" label="RangePicker" {...rangeConfig}>
+        <RangePicker />
+      </Form.Item>
+
+      <Form.Item name={['user', 'introduction']} label="Introduction">
+        <Input.TextArea />
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
