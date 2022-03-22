bd0de77f3e874f6e8b58d95db6e68cf3954198d0
Mingze Ma <mma26@sheffield.ac.uk>
2022-03-22 12:50:52 +0000

    web-app: resolve conflicts

--- web-app/src/actions/index.js
+++ web-app/src/actions/index.js
@@ -5,13 +5,10 @@
 
 import * as userActions from './userActions';
 import * as projectActions from './projectActions';
-
 import * as globalActions from './globalActions';
-import * as projectActions from './projectActions';
 
 export default {
   ...userActions,
   ...projectActions,
   ...globalActions,
-  ...projectActions,
 };
