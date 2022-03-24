8b216e4f71292efbd4eed2bfa13ab44195e49f2b
Shi Tianhao <tshi14@sheffield.ac.uk>
2022-03-24 14:35:13 +0000

    update edit profile

--- web-app/src/modules/account/profile/index.js
+++ web-app/src/modules/account/profile/index.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
-import {Avatar, Badge, FormGroup, Grid, styled, FormControl, TextField} from '@mui/material';
+import {Avatar, Badge, FormGroup, Grid, styled, FormControl, TextField, Box} from '@mui/material';
 import Stack from '@mui/material/Stack';
 import IconButton from '@mui/material/IconButton';
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
@@ -42,6 +42,13 @@ export default () => {
   const handleCancel = () => {
     setDisplay("none");
     setEditDisplay(null);
+  };
+
+  // @Todo Upload edit info
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = Object.fromEntries(new FormData(event.currentTarget));
+    console.log(data.name);
   };
 
   const handleChange = async (event) => {
@@ -117,76 +124,80 @@ export default () => {
         </Button>
       </Grid>
 
+
       <Grid item xs={3} display={display}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TextField
-              defaultValue={userInfo.name}
-              required
-              fullWidth
-              id="name"
-              label="Name"
-              name="name"
-              onChange={handleChange}
-              color={nameColor}
-              focused
-            />
-          </Grid>
-
-          <Grid item xs={12}>
-            <Autocomplete
-              defaultValue={userInfo.region}
-              disablePortal
-              fullWidth
-              id="region"
-              options={regionList}
-              renderInput={(params) => <TextField
-                {...params}
-                label="Select Region"
-                name="region"
-                onSelect={handleChange}
-                onClick={handleChange}
-                onInput={handleChange}
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={4}>
+            <Grid item xs={12}>
+              <TextField
+                defaultValue={userInfo.name}
+                required
+                fullWidth
+                id="name"
+                label="Name"
+                name="name"
                 onChange={handleChange}
-                onBlur={handleChange}
-                color={regionColor}
+                color={nameColor}
                 focused
-              />}
-            />
-          </Grid>
+              />
+            </Grid>
 
-          <Grid item xs={12}>
-            <Autocomplete
-              defaultValue={userInfo.currency_type}
-              disablePortal
-              fullWidth
-              id="currency"
-              options={currencyCode}
-              renderInput={(params) => <TextField
-                {...params}
-                label="Select Currency"
-                name="currency"
-                onSelect={handleChange}
-                onClick={handleChange}
-                onInput={handleChange}
-                onChange={handleChange}
-                onBlur={handleChange}
-                color={currencyColor}
-                focused
-              />}
-            />
+            <Grid item xs={12}>
+              <Autocomplete
+                defaultValue={userInfo.region}
+                disablePortal
+                fullWidth
+                id="region"
+                options={regionList}
+                renderInput={(params) => <TextField
+                  {...params}
+                  label="Select Region"
+                  name="region"
+                  onSelect={handleChange}
+                  onClick={handleChange}
+                  onInput={handleChange}
+                  onChange={handleChange}
+                  onBlur={handleChange}
+                  color={regionColor}
+                  focused
+                />}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <Autocomplete
+                defaultValue={userInfo.currency_type}
+                disablePortal
+                fullWidth
+                id="currency"
+                options={currencyCode}
+                renderInput={(params) => <TextField
+                  {...params}
+                  label="Select Currency"
+                  name="currency"
+                  onSelect={handleChange}
+                  onClick={handleChange}
+                  onInput={handleChange}
+                  onChange={handleChange}
+                  onBlur={handleChange}
+                  color={currencyColor}
+                  focused
+                />}
+              />
+            </Grid>
+            <Grid item xs={5}>
+              <Button disabled={saveDisabled} variant="contained" type="submit">
+                Save
+              </Button>
+            </Grid>
+            <Grid item xs={7}>
+              <Button variant="text" onClick={handleCancel}>
+                Cancel
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item xs={5}>
-            <Button disabled={saveDisabled} variant="contained">
-              Save
-            </Button>
-          </Grid>
-          <Grid item xs={7}>
-            <Button variant="text" onClick={handleCancel}>
-              Cancel
-            </Button>
-          </Grid>
-        </Grid>
+        </form>
+
       </Grid>
     </Grid>
   );
