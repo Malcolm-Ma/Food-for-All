d3d0259c7b2add082ce6f3b55a321466762b69d5
Mingze Ma <mma26@sheffield.ac.uk>
2022-03-30 21:31:57 +0100

    web-app: init donor modules

---
+++ web-app/src/components/ProjectCardList/ProjectCard.js
@@ -0,0 +1,14 @@
+/**
+ * @file project Card component
+ * @author Mingze Ma
+ */
+
+export default (props) => {
+  const {} = props;
+
+  return (
+    <div>
+      See in /material-kit-react/src/sections/@dashboard/products/ProductCard.js
+    </div>
+  );
+}
---
+++ web-app/src/modules/project/index.js
@@ -0,0 +1,21 @@
+/**
+ * @file project list for donors and guests
+ * @author Mingze Ma
+ */
+
+// module import
+import {} from 'react';
+
+// style import
+import './index.less';
+
+export default (props) => {
+
+  const {} = props;
+
+  return (
+    <div>
+      Project list for donors and guests
+    </div>
+  );
+}
