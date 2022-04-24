b36e85bfecbce1dbc69ba4164325f90937dc0027
DHT <hdong15@sheffield.ac.uk>
2022-04-24 17:18:47 +0100

    share1

--- web-app/src/modules/share/index.js
+++ web-app/src/modules/share/index.js
@@ -12,6 +12,8 @@ import Box from "@mui/material/Box";
 import Typography from '@mui/material/Typography';
 import Button from "@mui/material/Button";
 import * as React from "react";
+import {Image} from "antd";
+import success from 'src/modules/share/success.jpg'
 
 export default (props) => {
 
@@ -19,10 +21,11 @@ export default (props) => {
     <div align="center">
       <Box align="center" textAlign="center"
            sx={{width: '100%', maxWidth: 500}}>
+        <img src={success} alt="success" />
         <Typography variant="h3" component="div" gutterBottom>
-          You have successfully shared it with your friends.
+          You have successfully shared it with your friends. Thank you for sharing.
         </Typography>
-        
+
         <Button size="large" variant="outlined" href='project'>Confirm</Button>
       </Box>
     </div>
