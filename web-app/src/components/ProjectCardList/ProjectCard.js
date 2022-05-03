7db36a20ac1a60172e8fd4aa198f6339347c5455
DHT <hdong15@sheffield.ac.uk>
2022-05-03 22:04:49 +0100

    share page

--- web-app/src/components/ProjectCardList/ProjectCard.js
+++ web-app/src/components/ProjectCardList/ProjectCard.js
@@ -105,7 +105,7 @@ export default function ShowProjectCard(props) {
       </CardContent>
       <CardActions disableSpacing>
         <Button size="small" onClick={handleClickOpen}>Share</Button>
-        <Dialog open={openDialog} onClose={handleClose}>
+        <Dialog open={openDialog} onClose={handleClose} fullWidth={true} maxWidth={'xs'}>
           <DialogTitle>Share</DialogTitle>
           <DialogContent dividers={true}>
             <Typography variant="h6" sx={{pb: 2}}>
@@ -119,27 +119,8 @@ export default function ShowProjectCard(props) {
               }}
             />
           </DialogContent>
-          <DialogContent>
-            <Typography variant="h6">
-              Share By Email
-            </Typography>
-            <DialogContentText>
-              Please input the email of the friend you want to share,
-              we will send the project information to your friend.
-            </DialogContentText>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              label="Email Address"
-              type="email"
-              fullWidth
-              variant="standard"
-            />
-          </DialogContent>
           <DialogActions>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button onClick={handleClose} href='share'>Share</Button>
+            <Button onClick={handleClose}>OK</Button>
           </DialogActions>
         </Dialog>
         <Button size="small" href={`/donation/${project.pid}`}>Donate</Button>
--- web-app/src/modules/share/index.js
+++ web-app/src/modules/share/index.js
@@ -22,12 +22,52 @@ export default (props) => {
       <Box align="center" textAlign="center"
            sx={{width: '100%', maxWidth: 500}}>
         <img src={success} alt="success" />
-        <Typography variant="h3" component="div" gutterBottom>
-          You have successfully shared it with your friends. Thank you for sharing.
+        <Typography variant="h5" component="div" gutterBottom>
+            You have successfully donated!
+            Thank you very much. We hope you will be able to share this project with more people.
         </Typography>
 
         <Button size="large" variant="outlined" href='project'>Confirm</Button>
       </Box>
     </div>
   );
+    /**
+     * <Dialog open={openDialog} onClose={handleClose}>
+     *               <DialogTitle>Share</DialogTitle>
+     *               <DialogContent dividers={true}>
+     *                 <Typography variant="h6" sx={{pb: 2}}>
+     *                   Share By Twitter
+     *                 </Typography>
+     *                 <TwitterShareButton
+     *                     url={`${window.location.href}/${_.get(projectDetail, 'pid')}`}
+     *                     options={{
+     *                       size: 'large',
+     *                       text: 'Apex - Food For All',
+     *                     }}
+     *                 />
+     *               </DialogContent>
+     *               <DialogContent>
+     *                 <Typography variant="h6">
+     *                   Share By Email
+     *                 </Typography>
+     *                 <DialogContentText>
+     *                   Please input the email of the friend you want to share,
+     *                   we will send the project information to your friend.
+     *                 </DialogContentText>
+     *                 <TextField
+     *                     autoFocus
+     *                     margin="dense"
+     *                     id="name"
+     *                     label="Email Address"
+     *                     type="email"
+     *                     fullWidth
+     *                     variant="standard"
+     *                 />
+     *               </DialogContent>
+     *               <DialogActions>
+     *                 <Button onClick={handleClose}>Cancel</Button>
+     *                 <Button onClick={handleClose} href='share'>Share</Button>
+     *               </DialogActions>
+     *             </Dialog>
+     */
 }
