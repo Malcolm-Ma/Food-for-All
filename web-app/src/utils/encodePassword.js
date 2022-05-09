2668c7ad032ec4bedcab47e9e628af6ef1c7ec84
Mingze Ma <mma26@sheffield.ac.uk>
2022-05-09 20:44:08 +0100

    web-app: bug fixed in password encoding

--- web-app/src/utils/encodePassword.js
+++ web-app/src/utils/encodePassword.js
@@ -7,6 +7,8 @@ import _ from "lodash";
 export const ENCODE_KEY = '85';
 
 export const encode = (password = '') => {
-  const encodePassword = _.map(password, (str) => str.charCodeAt() ^ ENCODE_KEY).join('');
+  const encodePassword = _.map(password, (str) => {
+    return String.fromCharCode(str.charCodeAt() ^ ENCODE_KEY);
+  }).join('');
   return btoa(encodePassword);
 };
