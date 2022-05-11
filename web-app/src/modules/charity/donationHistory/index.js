139c652a8494c7c7be486a6d9705c1c3e2706868
Mingze Ma <mma26@sheffield.ac.uk>
2022-05-11 01:34:00 +0100

    web-app: charity donate history data source

--- web-app/src/modules/charity/donationHistory/index.js
+++ web-app/src/modules/charity/donationHistory/index.js
@@ -1,5 +1,9 @@
 import _ from "lodash";
-import {Table} from "antd";
+import { message, Table } from "antd";
+import { useEffect, useState } from "react";
+import actions from "src/actions";
+import moment from "moment";
+import { useSelector } from "react-redux";
 
 const columnsConfig = [
   {
@@ -22,9 +26,72 @@ const columnsConfig = [
     dataIndex: 'donate_num',
     key: 'donate_num',
   }
-  ]
+]
 
 export default () => {
+
+  const regionInfo = useSelector(state => state.global.regionInfo);
+  const { userInfo } = useSelector(state => state.user);
+
+  const [dataSource, setDataSource] = useState([]);
+
+  useEffect(() => {
+    if (!_.isEmpty(userInfo)) {
+      const fetchData = async () => {
+        try {
+          const userDonateHistory = _.get(userInfo, 'donate_history');
+          let projectHistoryDetail = [];
+          const projectPromiseAll = _.map(userDonateHistory, async (history, pid) => {
+            const { project_info: projectInfo } = await actions.getProjectInfo({
+              pid,
+              currency_type: regionInfo.currencyType,
+            });
+            const donationHistory = _.get(projectInfo, 'donate_history', []);
+            const fetchUserPromise = _.map(donationHistory, async (value, uid) => {
+              let result = {};
+              if (_.isEqual(uid, 'Anonymous')) {
+                _.set(result, 'name', 'Anonymous');
+              } else {
+                const { user_info: userRes } = await actions.getUserInfoById({ uid });
+                const { donate_history, project, type, ...otherProps } = userRes;
+                result = {
+                  ...result,
+                  ...otherProps,
+                };
+              }
+              _.map(value, (num, time) => {
+                projectHistoryDetail.push({
+                  ...result,
+                  timestamp: _.toNumber(time) * 1000,
+                  time: moment(_.toNumber(time) * 1000).format(),
+                  donate_num: _.toNumber(num),
+                  donate_amount: _.toNumber(num) * projectInfo.price,
+                  project_title: projectInfo.title,
+                  project_per_price: projectInfo.price,
+                  pid: projectInfo.pid,
+                  project_status: projectInfo.status,
+                  project_intro: projectInfo.intro,
+                });
+              });
+            });
+            await Promise.all(fetchUserPromise);
+          });
+          await Promise.all(projectPromiseAll).then(() => {
+            projectHistoryDetail = _.map(projectHistoryDetail, (item, key) => ({ ...item, key }))
+            setDataSource(_.sortBy(projectHistoryDetail, (o) => -o.timestamp));
+          });
+        } catch (e) {
+          message.error(e.name);
+        }
+      };
+      fetchData();
+    }
+  }, [userInfo, regionInfo.currencyType]);
+
+  useEffect(() => {
+    console.log('--dataSource--\n', dataSource);
+  }, [dataSource]);
+
   return (
     <Table
       columns={columnsConfig}
